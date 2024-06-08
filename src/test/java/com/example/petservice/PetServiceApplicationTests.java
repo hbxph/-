b532f797d869394dd9390de507f62409892f25cd
ovo559ii <2051833016@qq.com>
2024-06-08 22:06:25 +0800

    first

---
+++ src/test/java/com/example/petservice/PetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
