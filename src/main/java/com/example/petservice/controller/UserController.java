a6a26ea742906ea4ab5edf236422a391dc278d08
ovo559ii <2051833016@qq.com>
2024-06-13 22:14:35 +0800

    111

---
+++ src/main/java/com/example/petservice/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.example.petservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author aaa
+ * @since 2024-06-13
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}
+
---
+++ src/main/java/com/example/petservice/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.example.petservice.mapper;
+
+import com.example.petservice.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author aaa
+ * @since 2024-06-13
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
