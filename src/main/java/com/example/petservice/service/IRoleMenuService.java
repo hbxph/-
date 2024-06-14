40b9b61b22eb421ee1b95c4d31b344cac178aa5e
ovo559ii <2051833016@qq.com>
2024-06-14 10:33:13 +0800

    role menu

---
+++ src/main/java/com/example/petservice/service/IRoleMenuService.java
@@ -0,0 +1,16 @@
+package com.example.petservice.service;
+
+import com.example.petservice.entity.RoleMenu;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 角色菜单关系表 服务类
+ * </p>
+ *
+ * @author aaa
+ * @since 2024-06-14
+ */
+public interface IRoleMenuService extends IService<RoleMenu> {
+
+}
