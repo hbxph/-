7d0c450f4e5159f69e6f30fd55645804bc7a89fb
ovo559ii <2051833016@qq.com>
2024-06-21 11:26:56 +0800

    111

---
+++ src/main/java/com/example/petservice/controller/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package com.example.petservice.controller.dto;
+
+import com.example.petservice.entity.Menu;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * 接受前端登录请求的参数
+ */
+@Data
+public class UserDTO {
+    private String username;
+    private String password;
+    private String nickname;
+    private String avatarUrl;
+    private String token;
+    private String role;
+    private List<Menu> menus;
+}
